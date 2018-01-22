7adf78b9a2af82c9f087753d0faa28f67658f8c0
manu1222 <manu.sharma1222@gmail.com>
2018-01-22 22:26:33 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -25,11 +25,12 @@ socket.on("newLocationMessage", function (message) {
 
 $("#message-form").on("submit", function (e) {
     e.preventDefault();
+    var messageTextbox = $("[name=message]");
     socket.emit('createMessage', {
         from: "Manu",
-        text: $("[name=message]").val()
+        text: messageTextbox.val()
     }, function (data) {
-        console.log("gGot it", data);
+        messageTextbox.val('');
     });
 });
 
@@ -38,12 +39,15 @@ locationButton.on("click", function () {
     if (!navigator.geolocation) {
         return alert("Geolocation not supported by your browser.");
     }
+    locationButton.attr("disabled", "disabled").text("Sending location...");
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr("disabled").text("Send location");
         socket.emit("createLocationMessage", {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr("disabled").text("Send location");
         alert('Unable to fetch location');
     });
 });
