dbc79a81f6fb544cb5ec2b83652afda64323c04b
manu1222 <manu.sharma1222@gmail.com>
2018-01-22 17:57:16 +0530

    Geolocation to be on heroku

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,38 @@
 var socket = io();
-socket.on("connect", function() {
+socket.on("connect", function () {
     console.log("Connected to server");
 });
 
-socket.on("disconnect", function() {
+socket.on("disconnect", function () {
     console.log("Disconnected from server");
 });
 
-socket.on("newMessage", function(message) {
+socket.on("newMessage", function (message) {
     console.log("New message", message);
     var li = $("<li></li>");
     li.text(`${message.from}: ${message.text}`);
     $("#messages").append(li);
 });
 
-$("#message-form").on("submit", function(e) {
+$("#message-form").on("submit", function (e) {
     e.preventDefault();
     socket.emit('createMessage', {
         from: "Manu",
         text: $("[name=message]").val()
-    }, function(data) {
+    }, function (data) {
         console.log("gGot it", data);
+    });
+});
+
+var locationButton = $("#send-locaition");
+locationButton.on("click", function () {
+    if (!navigator.geolocation) {
+        return alert("Geolocation not supported by your browser.");
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position);
+    }, function () {
+        alert('Unable to fetch location');
     });
 });
