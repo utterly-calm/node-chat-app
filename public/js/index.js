131320c2610073f6e0beac432c02420eb69ab340
Vipul Sharma <vipul.sharma@trantorinc.com>
2018-01-23 13:44:32 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+
+function scrollToBottom() {
+    var messages = $("#messages");
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on("connect", function () {
     console.log("Connected to server");
 });
@@ -16,6 +30,7 @@ socket.on("newMessage", function (message) {
         createdAt: formattedTime
     });
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 socket.on("newLocationMessage", function (message) {
@@ -27,6 +42,7 @@ socket.on("newLocationMessage", function (message) {
         createdAt: formattedTime
     });
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 $("#message-form").on("submit", function (e) {
